6628daae82d861fb376d93e1d1c48a92b11be998
Alexis <alexis@gadbin.com>
2024-01-31 00:14:54 +0100

    Initial commit

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+const WelcomeScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+      <Text>WelcomeScreen</Text>
+    </View>
+  )
+}
+export default WelcomeScreen
